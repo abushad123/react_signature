0f3406c839f5d673e6dd928ddc6119218c7510c8
123Abushad <142307478+123Abushad@users.noreply.github.com>
2023-09-03 15:48:48 +0530

    some change

--- src/App.js
+++ src/App.js
@@ -17,8 +17,8 @@ const handleDateChane = (e) => {
     <div className="flex flex-col items-center mt-5 bg-slate-500 h-screen">
        <Title title={name} />
        <Title title={date} />
-        <input  type='text' value={"Enter Your Name"} className="m-3  w-96 text-center p-3" onChange={handleNameChane} />
-        <input  type='date' value={"Select Date"}  className="m-3  w-96 text-center p-3" onChange={handleDateChane} />
+        <input  type='text'  className="m-3  w-96 text-center p-3" onChange={handleNameChane} />
+        <input  type='date'   className="m-3  w-96 text-center p-3" onChange={handleDateChane} />
 
     </div>
   );
