be3ea8f15334487e6082f99fc0e42dea43cae7a1
123Abushad <142307478+123Abushad@users.noreply.github.com>
2023-09-03 14:26:56 +0530

    latest commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
+import Title from './components/Title';
+
+
 
 function App() {
+const  [name, setName] = useState("Abushad");
+const  [date, setDate] = useState("01/02/2023");
+const handleNameChane = (e) => {
+  setName(e.target.value);
+}
+
+const handleDateChane = (e) => {
+  setDate(e.target.value);
+}
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="flex flex-col items-center mt-5 bg-slate-500 h-screen">
+       <Title title={name} />
+       <Title title={date} />
+        <input  type='text' value={"Enter Your Name"} className="m-3  w-96 text-center p-3" onChange={handleNameChane} />
+        <input  type='date' value={"Select Date"}  className="m-3  w-96 text-center p-3" onChange={handleDateChane} />
+
     </div>
   );
 }
---
+++ src/components/Title.js
@@ -0,0 +1,10 @@
+
+function Title(props) {
+    return (
+      <div className="m-5 text-white w-96 text-center p-5 bg-slate-950">
+        <div>{props.title}</div>
+      </div>
+    );
+  }
+  
+  export default Title;
